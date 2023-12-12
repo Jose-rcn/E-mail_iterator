1527678d779ff5da130020b7a53b8a5d7855a0b0
José Ribeiro da costa Neto <josercn55@gmail.com>
2023-12-12 16:23:37 -0300

    Criação da pasta src e atualização da conexão e login do usuário no servidor smtp

--- src/main.py
+++ src/main.py
@@ -4,14 +4,13 @@ import smtplib
 smtp_server = 'mail.iff.edu.br'
 smtp_port = 587
 
-def connect_server(server, port):
-    with smtplib.SMTP(smtp_server, smtp_port) as server:
-        server.starttls()
-    return True
+
 
 while True:
     try:
-        print(connect_server(smtp_server,smtp_port))
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        print('Conectado ao servidor.')
     except Exception:
         print("Erro ao se conectar com o servidor. Verifique as informações do servidor")
         break
@@ -19,5 +18,11 @@ while True:
     user_username = input("Informe o seu endereço de e-mail: ")
     user_password = input('Informe a senha: ')
     print('-'*40)
-
-    server.login(username, password)
+    try:
+        server.login(user_username, user_password)
+        print('Usuário logado')
+    except smtplib.SMTPAuthenticationError:
+        print('E-mail ou senha incorretos')
+        continue
+    table_path = input('Informe o caminho para acessar o arquivo: ')
+    break
